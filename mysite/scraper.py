634725689a87c9adf15901e1b68c7ec6de618c6e
Anders Juengst <anders.juengst@gmail.com>
2018-09-16 20:14:31 -0400

    9-16-18 2

--- mysite/scraper.py
+++ mysite/scraper.py
@@ -1,26 +1,58 @@
 from teams.models import *
 from bs4 import BeautifulSoup
-import requests
+from django.conf import settings
+import requests, os, string
 
+soup = BeautifulSoup(open(os.path.join(settings.BASE_DIR, 'revolver.html')), 'html.parser')
 
 #page_url = "https://play.usaultimate.org/events/TCT-Pro-Championships-2018/schedule/Men/Club-Men/"
-#page = requests.get(page_url).content
-soup = BeautifulSoup(open('/Users/andersjuengst/Downloads/tctMens.html'), 'html.parser')
 
-
-def scrapePoolsPage(soup):
+def scrapePoolsPage(url):
+	soup = BeautifulSoup(open(os.path.join(settings.BASE_DIR, url)), 'html.parser')
 	pools = soup.find_all('div', 'pool')
 	numPools = len(pools)
-	teams = {}
+	teams = []
 	
-	print(len(pools) + ' pools')
+	print(str(len(pools)) + ' pools')
+	print(type(pools))
+
 	for p in pools:
 		teamlinks = p.find_all('a')
-		print(teamlinks[0])
+		for team in teamlinks:
+			link = team['href']
+			team_str = str(team)
+			seed = int(team_str.split('(')[1].split(')')[0])
+			name = team_str.split('>')[1].split('(')[0][:-1]
+			teams.append((name, seed, link))
+	return teams
+
+def scrapeTeamPage(url):
+	soup = BeautifulSoup(open(os.path.join(settings.BASE_DIR, url)), 'html.parser')
+	teamInfo = soup.find('div', 'profile_info')
+
+	city = teamInfo.find(id='CT_Main_0_dlCity')
+	city_str = str(city).split('>')[1].split('\t')[3][:-1]
+
+	competitionLevel = teamInfo.find(id="CT_Main_0_dlCompetitionLevel")
+	competitionLevel_str = str(competitionLevel).split('>')[4].split('\t')[4][:-4]
+
+	genderDivision = teamInfo.find(id="CT_Main_0_dlGenderDivision")
+	genderDivision_str = str(genderDivision).split('>')[4].split('\t')[4][:-4]
+
+	twitterLink = teamInfo.find(id="CT_Main_0_dlTwitter").find('a')['href']
+	twitterHandle = twitterLink.split('/')[-1]
+	print(city_str + ' ' + competitionLevel_str + ' ' + genderDivision_str + ' ' + twitterLink)
 
 
-	#print(soup.get_text())
 
-scrapePoolsPage(soup)
+
+def saveTeams(teams):
+	for team in teams:
+		pass
+
+soup = BeautifulSoup(open(os.path.join(settings.BASE_DIR, 'pony.html')), 'html.parser')
+
+#print(scrapePoolsPage("tctMens.html"))
+print(scrapeTeamPage('pony.html'))
 #scandal = Team(name="Scandal", city="Washington, D.C.", division=Division.WOMENS, twitterHandle="scandalultimate", twitterLink="http://www.twitter.com/scandalultimate/")
 #scandal.save()
---
+++ mysite/scraper_poc.py
@@ -0,0 +1,4 @@
+from teams.models import *
+
+ring = Team(name="Ring of Fire", city="Raleigh, N.C.", division='Open', twitterHandle="ringultimate", twitterLink="http://www.twitter.com/ringultimate/")
+ring.save()
